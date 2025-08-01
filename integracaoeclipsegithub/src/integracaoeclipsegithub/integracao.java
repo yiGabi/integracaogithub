68e0128bb57c606c92eab9d76574489f641d9d9f
Gabriele Peletti <gabriele.peletti@hotmail.com>
2025-08-01 11:26:45 -0300

    Update integracao.java

--- integracaoeclipsegithub/src/integracaoeclipsegithub/integracao.java
+++ integracaoeclipsegithub/src/integracaoeclipsegithub/integracao.java
@@ -5,6 +5,7 @@ public class integracao {
 	public static void main(String[] args) {
 		System.out.println("Primeira Integração");
 		System.out.println("Terceira Integração");
+		System.out.println("Testeteste");
 
 	}
 
