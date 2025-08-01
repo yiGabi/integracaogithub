a8cde9bd104504dd20e57eaea8cffdfc59057b94
yiGabi <gabriele.peletti@hotmail.com>
2025-08-01 11:24:18 -0300

    t

--- integracaoeclipsegithub/src/integracaoeclipsegithub/integracao.java
+++ integracaoeclipsegithub/src/integracaoeclipsegithub/integracao.java
@@ -4,7 +4,6 @@ public class integracao {
 
 	public static void main(String[] args) {
 		System.out.println("Primeira Integração");
-		System.out.println("Segunda Integração");
 		System.out.println("Terceira Integração");
 
 	}
